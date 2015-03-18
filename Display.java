d2a926496c8f4370c118875e17f6c9cb1afd58ea
Ohmoon <tsimmer9801@gmail.com>
2015-03-18 19:19:30 +0200

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,28 +1,25 @@
-package MinuMäng;
+package Projekt;
 
-import javax.swing.*; // teeme üldisemaks asendades import javax.swing.JFrame; ---->import javax.swing.*;
+import javax.swing.*; // teeme Ã¼ldisemaks asendades import javax.swing.JFrame; ---->import javax.swing.*;
 
 import java.awt.*; //Image jaoks
 
 public class Display extends JFrame {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	public JPanel gb = (JPanel) getGlassPane(); //lihtsalt vajalik...
 	public Images pilt;
-	public Keying võti;
+	public Keying v6ti;
 	
 	public Display() {
 		pilt = new Images();
 		gb.setVisible(true);
-		võti = new Keying(this, pilt);
+		v6ti = new Keying(this, pilt);
 		pilt.loadImages();
 		
 		gb.setLayout(new GridLayout(1, 1, 0, 0));
 		this.setLayout(new GridLayout(1, 1, 0, 0));
 		
-		gb.add(võti);
+		gb.add(v6ti);
 		this.add(pilt);
 	}
 }
