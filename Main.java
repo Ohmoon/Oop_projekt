237f0cf8fc2dc3a69855431ad2ce4be1a2f7514c
Ohmoon <tsimmer9801@gmail.com>
2015-03-18 19:20:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
-package MinuMäng;
+package Projekt;
 
 import javax.swing.JFrame;
 
-public class Main {
+public class Main{
+	
 	public static Display frame = new Display(); //teeme akna
 		public static int width = 600; //akna laius 
-		public static int height = 600; //akna kõrgus
+		public static int height = 600; //akna kÃµrgus
 		public static void main (String[] args){ //peaklass
 			frame.setSize(width, height);
-			frame.setResizable(true); //võimaldab akna mõõtmeid ise muuta hiljem
-			frame.setVisible(true); // aken on nähtav
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //võimaldab akent sulgeda
-			frame.setTitle("Space game something"); //mängu nimi
+			frame.setResizable(false); //vÃµimaldab akna mÃµÃµtmeid ise muuta hiljem
+			frame.setVisible(true); // aken on nÃ¤htav
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //vÃµimaldab akent sulgeda
+			frame.setTitle("Space game something"); //mÃ¤ngu nimi
 			frame.setLocationRelativeTo(null); //et aken ei ilmuks kogu aeg vasakule
 			frame.setAlwaysOnTop(true); //aken alati teise akna kohale, mitte peale
 		}
 }
 
-//Paljud read on vaja veel maha võtta, näiteks BLACK-kohad, mida pole enam vaja.
