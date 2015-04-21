ebc36169daa169cd15e39a53fcf26fb91b20d2a7
Ohmoon <tsimmer9801@gmail.com>
2015-04-21 15:24:35 +0300

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,25 +1,24 @@
 package Projekt;
 
-import javax.swing.*; // teeme Ã¼ldisemaks asendades import javax.swing.JFrame; ---->import javax.swing.*;
-
+import javax.swing.*; 
 import java.awt.*; //Image jaoks
 
 public class Display extends JFrame {
 	private static final long serialVersionUID = 1L;
-	public JPanel gb = (JPanel) getGlassPane(); //lihtsalt vajalik...
+	public JPanel ekraan = (JPanel) getGlassPane(); 
 	public Images pilt;
-	public Keying v6ti;
+	public Commands v6ti;
 	
 	public Display() {
 		pilt = new Images();
-		gb.setVisible(true);
-		v6ti = new Keying(this, pilt);
+		ekraan.setVisible(true);
+		v6ti = new Commands(this, pilt);
 		pilt.loadImages();
 		
-		gb.setLayout(new GridLayout(1, 1, 0, 0));
+		ekraan.setLayout(new GridLayout(1, 1, 0, 0));
 		this.setLayout(new GridLayout(1, 1, 0, 0));
 		
-		gb.add(v6ti);
+		ekraan.add(v6ti);
 		this.add(pilt);
 		
 		Sound.music();
