320ce8f0b14277ce5a0d7e877c34309e9714984e
Ohmoon <tsimmer9801@gmail.com>
2015-04-21 15:25:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,18 +4,17 @@ import javax.swing.JFrame;
 
 public class Main{
 	
-	public static Display frame = new Display(); //teeme akna
-		public static int width = 600; //akna laius 
-		public static int height = 600; //akna kÃµrgus
-		public static void main (String[] args){ //peaklass
+	public static Display frame = new Display(); 
+		public static int width = 600;  
+		public static int height = 600; 
+		public static void main (String[] args){ 
 			frame.setSize(width, height);
-			frame.setResizable(false); //vÃµimaldab akna mÃµÃµtmeid ise muuta hiljem
-			frame.setVisible(true); // aken on nÃ¤htav
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //vÃµimaldab akent sulgeda
-			frame.setTitle("Space game something"); //mÃ¤ngu nimi
-			frame.setLocationRelativeTo(null); //et aken ei ilmuks kogu aeg vasakule
-			frame.setAlwaysOnTop(true); //aken alati teise akna kohale, mitte peale
+			frame.setResizable(false); 
+			frame.setVisible(true); 
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+			frame.setTitle("Space game something"); 
+			frame.setLocationRelativeTo(null); 
+			frame.setAlwaysOnTop(true); 
 		}
 		
 }
-
