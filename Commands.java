93a75efbeee2f6189b88088ae86a0b1679479637
Ohmoon <tsimmer9801@gmail.com>
2015-04-21 15:25:34 +0300

    Update and rename Keying.java to Commands.java

--- Commands.java
+++ Commands.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 
 
-public class Keying extends JPanel  {
+public class Commands extends JPanel  {
 	
 	public static int Random_koht(int min, int max, Random random){
 	    int range = max - min + 1;
@@ -26,13 +26,12 @@ public class Keying extends JPanel  {
 	public Rectangle character;
 	public Rectangle kuul;
 	public Rectangle meteoor;
+	
 
 	public int charL = 24;
 	public int charP = 36;
 	Random random = new Random();
 
-
-	//public float jumpingTime = 200;
 
 	public boolean right = false;
 	public boolean left = false;
@@ -40,12 +39,15 @@ public class Keying extends JPanel  {
 	public boolean down = false;
 	public boolean shooting = false;
 	
-	public Keying(Display frame, Images pilt) {
+	
+	public Commands(Display frame, Images pilt) {
 		character = new Rectangle(290, 500, charL, charP);
-		kuul = new Rectangle(10,10,100,100);
-		meteoor = new Rectangle(Random_koht(1, 500, random),10,10,10);
+		kuul = new Rectangle(character.x,character.y,100,100);
+		meteoor = new Rectangle(Random_koht(1, 500, random),-10,40,90);
+	
+		
 
-		frame.addKeyListener(new KeyAdapter() { //objekti liigutamine
+		frame.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 
 				if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -93,7 +95,7 @@ public class Keying extends JPanel  {
 				if(e.getKeyCode() == KeyEvent.VK_SPACE) {
 					shooting = false;
 				}
-				
+				 
 			}
 
 		});
@@ -102,11 +104,18 @@ public class Keying extends JPanel  {
 
 	public void paintComponent(Graphics g) {
 		if(Main.frame.pilt.imagesLoaded) {
+			
+			if (meteoor.y > 530){
+				meteoor = new Rectangle(Random_koht(1, 500, random),10,60,80);
+				
+				}
+			
 			//	timer = new Timer(400, (ActionListener) this);
 			//     timer.start();
 			g.drawImage(Main.frame.pilt.taust, 0, 0, 1920, 1080, null);
 			g.drawImage(Main.frame.pilt.laev, character.x, character.y, 64, 64, null);
-			g.drawImage(Main.frame.pilt.meteoor, meteoor.x,meteoor.y,100,100,null);
+			g.drawImage(Main.frame.pilt.meteoor, meteoor.x,meteoor.y,160,160,null);
+			
 			meteoor.y+= 1;
 			
 			
@@ -125,10 +134,8 @@ public class Keying extends JPanel  {
 				character.y -= 1;
 			}
 			if(shooting){
-				g.drawImage(Main.frame.pilt.kuul, character.x+27, character.y-10,10,10, null );
-				if (kuul.y>0){
-					kuul.y--;
-				}
+				g.drawImage(Main.frame.pilt.kuul, kuul.x+27, kuul.y-10,10,10, null );
+				kuul.y-= 1;
 				try {
 					Sound.music1();
 				} catch (MalformedURLException e) {
@@ -138,9 +145,32 @@ public class Keying extends JPanel  {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+		
 			}
+			Rectangle r1 = character.getBounds();
+			Rectangle r2 = meteoor.getBounds();
+			if (r1.intersects(r2)){
+				try {
+					Sound.plahvatus();
+					Thread.sleep(500);
 
-			repaint(); //vÃ¤ltida juhust, kus objekt ei hakka liikuma
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				//g.drawImage(Main.frame.pilt.gameover, 200,180,200,200,null);
+				
+			System.exit(0);
+				
+					
+							
+			}
+		
+		
+			repaint(); 
 		}
 	}
 
