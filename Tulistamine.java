6a1fc34a2cacc8d652bf3ba287564bd7b61449bd
Ohmoon <tsimmer9801@gmail.com>
2015-04-21 16:39:01 +0300

    Create Tulistamine.java

---
+++ Tulistamine.java
@@ -0,0 +1,23 @@
+package Projekt;
+import java.awt.Image;
+
+import javax.swing.*;
+
+public class Tulistamine extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Image kuul;
+
+	
+	public Tulistamine(double x, double y){
+		kuul = new ImageIcon("C:/Oop/bullet.png").getImage();
+		
+
+	}
+	
+	
+	
+}
