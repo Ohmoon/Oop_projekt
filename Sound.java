a3eba613ed7b2df493470baf2fe828149bbe10bb
Ohmoon <tsimmer9801@gmail.com>
2015-04-21 15:26:44 +0300

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -11,20 +11,27 @@ import javax.swing.*;
 
 public class Sound extends JPanel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public static void music() {
 		AudioPlayer AP = AudioPlayer.player;
 		AudioStream AS;
 		AudioData AD;
 
 		sun.audio.ContinuousAudioDataStream loop = null;
-
+		//if (Main.frame.isVisible()) {
 		try{
 			AS = new AudioStream( new FileInputStream("soundmusa.wav"));
 			AD = AS.getData();
 			loop = new sun.audio.ContinuousAudioDataStream(AD);
 		}catch(IOException error){}
-		AP.start(loop);
-	}
+		AP.start(loop);}
+		
+	//}
+	
 	public static void music1() throws MalformedURLException, InterruptedException{
 		URL url = new URL("file:/C:/Oop/Projekt/LASER.wav");
 		AudioClip clip = Applet.newAudioClip(url);
@@ -32,6 +39,12 @@ public class Sound extends JPanel {
 		Thread.sleep(100);
 	}
 	
-	
+	public static void plahvatus() throws MalformedURLException, InterruptedException{
+		URL url2 = new URL("file:/C:/Eclipse/Projects/projekt/plahvatus.wav"); //miski p2rast ma ei saanud ilma url'ita, dunno why
+		AudioClip clip = Applet.newAudioClip(url2);
+		clip.play();
+		Thread.sleep(2000);
+	}
+
 
 }
